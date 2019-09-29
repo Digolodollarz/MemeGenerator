f2251f2a0535c91767babbd11cb65a6dd130b3b3
Eddie <eddie@diggle.tech>
2019-09-29 21:25:23 +0200

    Initial commit

---
+++ app/src/main/java/tech/diggle/apps/meme/generator/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package tech.diggle.apps.meme.generator.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
